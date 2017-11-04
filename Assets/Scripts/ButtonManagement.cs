0dc47d8e23addea20c84960e9a190480b4da5142
SzJoakim93 <szabo.joakim@gmail.com>
2017-11-04 15:05:00 +0100

    Kezdő verzió a pac-man klónból.

---
+++ Assets/Scripts/ButtonManagement.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonManagement : MonoBehaviour {
+
+	// Use this for initialization
+	public void new_game_btn() {
+		Application.LoadLevel ("ingame");
+	}
+
+	public void quit_btn() {
+		Application.Quit();
+	}
+}
