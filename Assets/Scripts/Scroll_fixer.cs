e38c2ab2002cef52f480e24a5f0298ba31f2c212
SzJoakim93 <szabo.joakim@gmail.com>
2020-05-11 23:00:46 +0200

    Save state of level menus at exit of game. Change screen orientation.

--- Assets/Scripts/Scroll_fixer.cs
+++ Assets/Scripts/Scroll_fixer.cs
@@ -9,12 +9,14 @@ public class Scroll_fixer : MonoBehaviour {
 	public bool vertical;
 
 	float movement_range;
+	int levelState;
     public float packages;
 
 	float movement_level;
 	float next_level;
     float init_level;
     float end_level;
+	public string key;
 
 	// Use this for initialization
     void Start()
@@ -24,6 +26,12 @@ public class Scroll_fixer : MonoBehaviour {
 			movement_range = rectTransform.position.x*2;
             movement_level = next_level = init_level = rectTransform.position.x;
             end_level = init_level - movement_range * packages;
+			levelState = PlayerPrefs.GetInt(key, 0);
+			if (levelState != 0) {
+				movement_level = next_level += levelState * movement_range;
+				rectTransform.position = new Vector3(movement_level, rectTransform.position.y, rectTransform.position.z);
+			}
+
         }
     }
 
@@ -33,10 +41,15 @@ public class Scroll_fixer : MonoBehaviour {
 		if (horizontal) {
 
 			if (next_level >= movement_level - 1.0f && next_level <= movement_level + 1.0f) {
-				if (next_level < init_level && rectTransform.position.x >  movement_level + 50.0f)
+				if (next_level < init_level && rectTransform.position.x >  movement_level + 50.0f) {
 					next_level += movement_range;
-				else if (next_level > end_level && rectTransform.position.x <  movement_level - 50.0f)
+					levelState++;
+				}
+				else if (next_level > end_level && rectTransform.position.x <  movement_level - 50.0f) {
 					next_level -= movement_range;
+					levelState--;
+				}
+					
 			}
 
 			if (rectTransform.position.x <  next_level + 10.0f && rectTransform.position.x >  next_level - 10.0f)
@@ -51,12 +64,20 @@ public class Scroll_fixer : MonoBehaviour {
 	}
 
 	public void leftClick() {
-		if (next_level < init_level)
-		next_level += movement_range;
+		if (next_level < init_level) {
+			next_level += movement_range;
+			levelState++;
+		}	
 	}
 
 	public void rightClick() {
-		if (next_level > end_level)
-		next_level -= movement_range;
+		if (next_level > end_level) {
+			next_level -= movement_range;
+			levelState--;
+		}
+	}
+
+	public void saveState() {
+		PlayerPrefs.SetInt(key, levelState);
 	}
 }
